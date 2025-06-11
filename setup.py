3f691df8b0a3242bc688ed4219e61859426796ca
xiaoqiang.yxq <yuxiaoqiang@rexense.com>
2025-06-11 13:49:04 +0800

    Modified energy unit from Wh to kWh

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (HERE / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="aiorexense",
-    version="0.1.0",
+    version="0.1.3",
     description="Rexense device client library with HTTP API and WebSocket support",
     long_description=long_description,
     long_description_content_type="text/markdown",
